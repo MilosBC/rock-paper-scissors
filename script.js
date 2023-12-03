5bf4dfe723affd5037b3ae61ce8a08ba7b8ee664
Milos Stefanov <144391186+MilosBC@users.noreply.github.com>
2023-12-04 00:15:27 +0100

    Complete game logic
    The game can now be played in the UI

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-console.log(`Welcome to Rock Paper Scissors! 😎  You have only 5 chances to beat the computer, good luck!`);
-
-
-
+'use strict';
 
 let round = 1;
 let playerScore = 0;
 let computerScore = 0;
 
+let playerSelection;
+let computerSelection;
+
+let playerScoreText = document.querySelector('.player-score');
+let computerScoreText = document.querySelector('.computer-score');
+let roundCounter = document.querySelector('.round-counter');
+
+const btnRock = document.querySelector('.btnRock');
+const btnPaper = document.querySelector('.btnPaper');
+const btnScissors = document.querySelector('.btnScissors');
+const btnPlayAgain = document.querySelector('.play-again');
+
+let playerImage = document.querySelector('.player-image');
+let computerImage = document.querySelector('.computer-image');
+
+
+let gameMessage = document.querySelector('.game-message');
+
+const leftDiv = document.querySelector('.left');
+const rightDiv = document.querySelector('.right');
+
+
+
+
+
 
 
 
 function getComputerChoice() {
-    // I will use Math.random() function to generate random numbers between 0 and 3. If the number is greater than 0 and less than or equal to 1 - the function returns "rock". If the number is greater than 1 and less than or equal to 2 - the function returns "paper". Lastly, if the number is greater than 2 and less than or equal to 3 - the function returns "scissors".
+    
+    const generateRandomNumber = Math.trunc(Math.random() * 3) + 1;
 
-
-
-
-    const generateRandomNumber = Math.random() * 3;
-
-
-
-
-
-
-    if (generateRandomNumber > 0 && generateRandomNumber <= 1) {
+    if (generateRandomNumber === 1) {
 
 
         return "rock";
-    } else if (generateRandomNumber > 1 && generateRandomNumber <= 2) {
+    } else if (generateRandomNumber === 2) {
 
 
         return "paper";
-    } else if (generateRandomNumber > 2 && generateRandomNumber <= 3) {
+    } else if (generateRandomNumber === 3) {
 
 
         return "scissors";
@@ -48,104 +61,175 @@ function getComputerChoice() {
 
 
 
-
-
-
-
-
-//declaration of player's choice"
-let playerSelection;
-
-
-
-
-//declaration of player's choice"
-let computerSelection;
-
-
-
-
-//the logic for playing a single round of the game
-
-
-
-
 function playRound(playerSelection, computerSelection) {
 
 
-
-
-
-
-    if (playerSelection == "rock" && computerSelection == "rock") {
-        console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...no one scores! 😑
-
-
-
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        /*console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...no one scores! 😑
 
    Player score: ${playerScore}
-   Computer score: ${computerScore}`)
+   Computer score: ${computerScore}`) */
+
+   roundCounter.textContent = `Round ${round}`
+
+   playerImage.src = `rock.JPG`
+   computerImage.src = `rock.JPG`;
+
+   gameMessage.textContent = `No one scores!`;
+
+   playerScoreText.textContent = playerScore;
+   computerScoreText.textContent = computerScore;
+
+
+
+
+
     } else if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...computer scores! 😱
+        /*console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...computer scores! 😱
 
 
 
 
    Player score: ${playerScore}
-   Computer score: ${++computerScore}`)
+   Computer score: ${++computerScore}`) */
+
+   roundCounter.textContent = `Round ${round}`
+
+   playerImage.src = `rock.JPG`
+   computerImage.src = `paper.JPG`;
+
+   gameMessage.textContent = `Computer scores!`;
+
+   playerScoreText.textContent = playerScore;
+   ++computerScore;
+   computerScoreText.textContent = computerScore;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...you score! 😄
+        /*console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...you score! 😄
 
 
 
 
    Player score: ${++playerScore}
-   Computer score: ${computerScore}`)
+   Computer score: ${computerScore}`) */
+
+   roundCounter.textContent = `Round ${round}`
+
+   playerImage.src = `rock.JPG`
+   computerImage.src = `scissors.JPG`;
+
+   gameMessage.textContent = `Player scores!`;
+   ++playerScore;
+   playerScoreText.textContent = playerScore;
+   computerScoreText.textContent = computerScore;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...you score! 😄
+         /*console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...you score! 😄
 
 
 
 
    Player score: ${++playerScore}
-   Computer score: ${computerScore}`)
+   Computer score: ${computerScore}`) */
+
+   roundCounter.textContent = `Round ${round}`
+
+   playerImage.src = `paper.JPG`
+   computerImage.src = `rock.JPG`;
+
+   gameMessage.textContent = `Player scores!`;
+   ++playerScore;
+   playerScoreText.textContent = playerScore;
+   computerScoreText.textContent = computerScore;
     } else if (playerSelection === "paper" && computerSelection === "paper") {
-        console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...no one scores! 😑
+       /* console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...no one scores! 😑
 
 
 
 
    Player score: ${playerScore}
-   Computer score: ${computerScore}`)
+   Computer score: ${computerScore}`) */
+
+   roundCounter.textContent = `Round ${round}`
+
+   playerImage.src = `paper.JPG`
+   computerImage.src = `paper.JPG`;
+
+   gameMessage.textContent = `No one scores!`;
+
+   playerScoreText.textContent = playerScore;
+   computerScoreText.textContent = computerScore;
+
+
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...computer scores! 😱
+       /* console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...computer scores! 😱
 
 
 
 
    Player score: ${playerScore}
-   Computer score: ${++computerScore}`)
+   Computer score: ${++computerScore}`) */
+
+   roundCounter.textContent = `Round ${round}`
+
+   playerImage.src = `paper.JPG`
+   computerImage.src = `scissors.JPG`;
+
+   gameMessage.textContent = `Computer scores!`;
+
+   playerScoreText.textContent = playerScore;
+   ++computerScore;
+   computerScoreText.textContent = computerScore;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...computer scores! 😱
+        /*console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...computer scores! 😱
  
        Player score: ${playerScore}
-       Computer score: ${++computerScore}`)
+       Computer score: ${++computerScore}`) */
+
+       roundCounter.textContent = `Round ${round}`
+
+       playerImage.src = `scissors.JPG`
+       computerImage.src = `rock.JPG`;
+    
+       gameMessage.textContent = `Computer scores!`;
+    
+       playerScoreText.textContent = playerScore;
+       ++computerScore;
+       computerScoreText.textContent = computerScore;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...you score! 😄
+        /*console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...you score! 😄
 
 
 
 
    Player score: ${++playerScore}
-   Computer score: ${computerScore}`)
+   Computer score: ${computerScore}`) */
+
+   roundCounter.textContent = `Round ${round}`
+
+   playerImage.src = `scissors.JPG`
+   computerImage.src = `paper.JPG`;
+
+   gameMessage.textContent = `Player scores!`;
+   ++playerScore;
+   playerScoreText.textContent = playerScore;
+   computerScoreText.textContent = computerScore;
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...no one scores! 😑
+       /* console.log(`Round ${round}: Player chooses ${playerSelection}, computer chooses ${computerSelection}...no one scores! 😑
 
 
 
 
        Player score: ${playerScore}
-       Computer score: ${computerScore}`)
+       Computer score: ${computerScore}`) */
+
+       roundCounter.textContent = `Round ${round}`
+
+       playerImage.src = `scissors.JPG`
+       computerImage.src = `scissors.JPG`;
+    
+       gameMessage.textContent = `No one scores!`;
+    
+       playerScoreText.textContent = playerScore;
+       computerScoreText.textContent = computerScore;
     }
 
 
@@ -165,57 +249,154 @@ function playRound(playerSelection, computerSelection) {
 
 
 
-// the function that lets us play the game exactly five times
 
 
 
 
-function game() {
+
+/*function game() {
 
 
 
 
-    for (let i = 0; i <= 4; i++) {
-        playerSelection = prompt("Rock, paper or scissors - what do you choose?").toLowerCase();
-        computerSelection = getComputerChoice();
-
+   
+       
+       
 
         playRound(playerSelection, computerSelection);
 
+    
+
 
     }
+
 
 
 
 
     if (playerScore > computerScore) {
-        console.log(`You are the winner! 🏆
-     
-       Final score
-     
-       Player score: ${playerScore}
-       Computer score: ${computerScore}`)
+        roundCounter.textContent = `Round ${round}`
+        playerImage.src = `trophy.png`
+        computerImage.src = 'crying.png'
+        gameMessage.textContent = `Player wins the game!`;
+    
+        playerScoreText.textContent = playerScore;
+        computerScoreText.textContent = computerScore;
     } else if (playerScore < computerScore) {
-        console.log(`Computer is the winner! 🏆
-     
-       Final score
-     
-       Player score: ${playerScore}
-       Computer score: ${computerScore}`)
-    } else {
-        console.log(`Tie, no winner! 🤷‍♂️
-     
-       Final score
-     
-       Player score: ${playerScore}
-       Computer score: ${computerScore}`)
+        roundCounter.textContent = `Round ${round}`
+        playerImage.src = `crying.png`
+        computerImage.src = 'trophy.png'
+        gameMessage.textContent = `Computer wins the game!`;
+    
+        playerScoreText.textContent = playerScore;
+        computerScoreText.textContent = computerScore;
     }
-}
+    
+    btnPlayAgain.classList.remove('hidden'); */
+
+
+// Event handlers for the game
+
+btnRock.addEventListener('click', function() {
+    playerSelection = `rock`;
+    computerSelection = getComputerChoice();
+    leftDiv.classList.remove('hidden');
+    rightDiv.classList.remove('hidden');
+
+    playRound(playerSelection, computerSelection);
+
+    if (playerScore === 5) {
+        playerImage.src = `trophy.png`
+        computerImage.src = 'crying.png'
+        gameMessage.textContent = `Player wins the game!`;
+        btnRock.classList.add('hidden');
+        btnPaper.classList.add('hidden');
+        btnScissors.classList.add('hidden');
+        btnPlayAgain.classList.remove('hidden');
+    } else if (computerScore === 5) {
+        playerImage.src = `crying.png`
+        computerImage.src = 'trophy.png'
+        gameMessage.textContent = `Computer wins the game!`;
+        btnRock.classList.add('hidden');
+        btnPaper.classList.add('hidden');
+        btnScissors.classList.add('hidden');
+        btnPlayAgain.classList.remove('hidden');
+
+    }
+    
+    
+});
+
+btnPaper.addEventListener('click', function() {
+    playerSelection = `paper`;
+    computerSelection = getComputerChoice();
+    leftDiv.classList.remove('hidden');
+    rightDiv.classList.remove('hidden');
+
+    playRound(playerSelection, computerSelection);
+
+    if (playerScore === 5) {
+        playerImage.src = `trophy.png`
+        computerImage.src = 'crying.png'
+        gameMessage.textContent = `Player wins the game!`;
+        btnRock.classList.add('hidden');
+        btnPaper.classList.add('hidden');
+        btnScissors.classList.add('hidden');
+        btnPlayAgain.classList.remove('hidden');
+    } else if (computerScore === 5) {
+        playerImage.src = `crying.png`
+        computerImage.src = 'trophy.png'
+        gameMessage.textContent = `Computer wins the game!`;
+        btnRock.classList.add('hidden');
+        btnPaper.classList.add('hidden');
+        btnScissors.classList.add('hidden');
+        btnPlayAgain.classList.remove('hidden');
+
+    }
+   
+    
+});
+
+btnScissors.addEventListener('click', function() {
+    playerSelection = `scissors`;
+    computerSelection = getComputerChoice();
+    leftDiv.classList.remove('hidden');
+    rightDiv.classList.remove('hidden');
+
+    playRound(playerSelection, computerSelection);
+
+    if (playerScore === 5) {
+        playerImage.src = `trophy.png`
+        computerImage.src = 'crying.png'
+        gameMessage.textContent = `Player wins the game!`;
+        btnRock.classList.add('hidden');
+        btnPaper.classList.add('hidden');
+        btnScissors.classList.add('hidden');
+        btnPlayAgain.classList.remove('hidden');
+    } else if (computerScore === 5) {
+        playerImage.src = `crying.png`
+        computerImage.src = 'trophy.png'
+        gameMessage.textContent = `Computer wins the game!`;
+        btnRock.classList.add('hidden');
+        btnPaper.classList.add('hidden');
+        btnScissors.classList.add('hidden');
+        btnPlayAgain.classList.remove('hidden');
+
+    }
+
+    
+   
+});
 
 
 
 
-game();
+
+
+
+
+
+
 
 
 
